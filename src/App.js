50877cb9e9e8d5041f593f6382d06d47f12ca250
sidashen <shashasi8023@163.com>
2020-03-16 21:32:06 +0800

    [shasha] feat: currency input at the page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CurrencyInput from './component/CurrencyInput'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <CurrencyInput
+          scale="u" />
+        <CurrencyInput
+          scale="c" />
+      </div>
+    );
+  }
 }
 
 export default App;
